5785634c74d2757b4fe98f93dbabaa305c21222a
Absox <xabsox@gmail.com>
2016-04-20 14:00:08 -0400

    Prototype implementation of hashqueue.

--- src/main/java/com/goatdev/auth/AuthTokenManager.java
+++ src/main/java/com/goatdev/auth/AuthTokenManager.java
@@ -1,10 +1,9 @@
 package com.goatdev.auth;
 
+import com.goatdev.user.User;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Authentication token management component.
@@ -19,5 +18,42 @@ public class AuthTokenManager {
     public AuthTokenManager() {
         tokenMap = new HashMap<>();
         expirationQueue = new LinkedList<>();
+    }
+
+    /**
+     * Issues an auth token.
+     * @param user User to issue token for.
+     * @return Auth token issued.
+     */
+    public AuthToken issueToken(User user) {
+        clearExpiredTokens();
+
+        AuthToken newToken = new AuthToken(
+                new Date(System.currentTimeMillis() + 1000 * 60 * 60),
+                UUID.randomUUID().toString(), user);
+        tokenMap.put(newToken.getValue(), newToken);
+        expirationQueue.add(newToken);
+        return newToken;
+    }
+
+    public AuthToken authenticate(String authTokenValue) {
+        clearExpiredTokens();
+
+        if (tokenMap.containsKey(authTokenValue)) {
+            AuthToken oldToken = tokenMap.remove(authTokenValue);
+            return issueToken(oldToken.getUser());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Clears all expired tokens.
+     */
+    private void clearExpiredTokens() {
+        while (expirationQueue.peek() != null && expirationQueue.peek().getExpiration().after(new Date())) {
+            AuthToken toRemove = expirationQueue.remove();
+            if (tokenMap.containsKey(toRemove.getValue())) tokenMap.remove(toRemove.getValue());
+        }
     }
 }
---
+++ src/main/java/com/goatdev/util/HashQueue.java
@@ -0,0 +1,98 @@
+package com.goatdev.util;
+
+import java.util.HashMap;
+
+/**
+ * A queue with a hashmap.
+ * Created by ran on 4/20/16.
+ */
+public class HashQueue<KeyType, DataType> {
+
+    private HashMap<KeyType, Node<KeyType, DataType>> nodeMapping;
+    private Node<KeyType, DataType> head;
+    private Node<KeyType, DataType> tail;
+
+    private int size;
+
+    public HashQueue() {
+        nodeMapping = new HashMap<>();
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public void enqueue(KeyType key, DataType data) throws Exception {
+        if (nodeMapping.containsKey(key)) throw new Exception("Attempting to insert duplicate key into map.");
+
+        Node<KeyType, DataType> newNode = new Node<>(key, data);
+
+        // If we're empty, populate the pointers.
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            // Otherwise, we do this.
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+        nodeMapping.put(key, newNode);
+        size++;
+    }
+
+    public DataType peek() {
+        if (head != null) return head.data;
+        return null;
+    }
+
+    public DataType dequeue() {
+        if (head == null) return null;
+
+        size--;
+        Node<KeyType, DataType> toRemove = head;
+
+        if (tail == head) tail = null;
+        head = head.next;
+        if (head != null) head.previous = null;
+
+        nodeMapping.remove(toRemove.key);
+        return toRemove.data;
+    }
+
+    public boolean containsKey(KeyType key) {
+        return nodeMapping.containsKey(key);
+    }
+
+    public DataType remove(KeyType key) {
+        Node<KeyType, DataType> toRemove = nodeMapping.remove(key);
+        if (toRemove == null) return null;
+
+        size--;
+        if (head == toRemove) head = toRemove.next;
+        if (tail == toRemove) tail = toRemove.previous;
+
+        if (toRemove.previous != null) toRemove.previous.next = toRemove.next;
+        if (toRemove.next != null) toRemove.next.previous = toRemove.previous;
+
+        return null;
+    }
+
+    private static class Node<KeyType, DataType> {
+        Node<KeyType, DataType> previous;
+        Node<KeyType, DataType> next;
+        KeyType key;
+        DataType data;
+
+        Node(KeyType key, DataType data) {
+            this.key = key;
+            this.data = data;
+            previous = null;
+            next = null;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
